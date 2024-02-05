1ed705acf231378663a53497b52310a09ff6545d
Jack Tysoe <jack@tys.one>
2024-02-05 21:34:13 +0000

    stash

--- run.sh
+++ run.sh
@@ -8,51 +8,10 @@ export KPAT=$(aws secretsmanager get-secret-value --secret-id ${KONNECT_PAT_SECR
 # output each entry as a single line json
 readarray CONTROL_PLANES < <(yq -o=j -I=0 '.control_planes[]' control-planes.yaml )
 
-
-if [ "$MODE" == "plan" ]
-then
-  for CONTROL_PLANE in "${CONTROL_PLANES[@]}";
-  do
-    # identity mapping is a single json snippet representing a single entry
-    export name=$(echo "$CONTROL_PLANE" | yq '.name' -)
-    export desc=$(echo "$CONTROL_PLANE" | yq '.description' -)
-    export aws_account=$(echo "$CONTROL_PLANE" | yq '.aws_account' -)
-    export aws_region=$(echo "$CONTROL_PLANE" | yq '.aws_region' -)
-    export ecs_cluster=$(echo "$CONTROL_PLANE" | yq '.ecs_cluster' -)
-    
-    echo ""
-    echo "> Looking up control-plane: $name"
-    export AUTH_STATUS=$(curl -s -o /dev/null -w "%{http_code}" --request GET \
-      --header 'Accept: application/json' \
-      --header "Authorization: Bearer ${KPAT}" \
-      --url "https://${KONNECT_REGION}.api.konghq.com/v2/control-planes?filter%5Bname%5D%5Beq%5D=${name}")
-
-    if [[ "$AUTH_STATUS" == 401 ]]
-    then
-      echo "!! Konnect PAT token is invalid, missing, or expired"
-      exit 1
-    fi
-
-    export FOUND_STATUS=$(curl -s --request GET \
-      --header 'Accept: application/json' \
-      --header "Authorization: Bearer ${KPAT}" \
-      --url "https://${KONNECT_REGION}.api.konghq.com/v2/control-planes?filter%5Bname%5D%5Beq%5D=${name}" | yq -P e '.meta.page.total')
-
-    if [[ "$FOUND_STATUS" < 1 ]]
-    then
-      echo "> Control Plane $name does not exist - would create it..."
-
-      export MESSAGE="⚠️ -> Create control plane '$name'"
-    else
-      export MESSAGE="✅ -> Found '$name'"
-    fi
-  done
-
-  echo -e "\`\`\`$MESSAGE\`\`\`" > out.txt
-fi
-
 if [ "$MODE" == "apply" ]
 then
+  echo -e "- control_planes:\n" > control_planes_konnect.json
+
   for CONTROL_PLANE in "${CONTROL_PLANES[@]}";
   do
     # identity mapping is a single json snippet representing a single entry
@@ -61,7 +20,7 @@ then
     export aws_account=$(echo "$CONTROL_PLANE" | yq '.aws_account' -)
     export aws_region=$(echo "$CONTROL_PLANE" | yq '.aws_region' -)
     export ecs_cluster=$(echo "$CONTROL_PLANE" | yq '.ecs_cluster' -)
-    
+
     echo ""
     echo "> Looking up control-plane: $name"
     export AUTH_STATUS=$(curl -s -o /dev/null -w "%{http_code}" --request GET \
@@ -117,11 +76,10 @@ EOF
     curl -s --request GET \
       --header 'Accept: application/json' \
       --header "Authorization: Bearer ${KPAT}" \
-      --url "https://${KONNECT_REGION}.api.konghq.com/v2/control-planes?filter%5Bname%5D%5Beq%5D=${name}" \
-      >> control_planes_konnect.json
-
-    # cat control_planes_konnect.json
-    # DO TERRAFORM APPLY STUFF HERE
+      --url "https://${KONNECT_REGION}.api.konghq.com/v2/control-planes?filter%5Bname%5D%5Beq%5D=${name}" |
+      yq -p=json - >> control_planes_konnect.json
 
   done
+
+  
 fi
--- test.Dockerfile
+++ test.Dockerfile
@@ -2,11 +2,14 @@ FROM ubuntu:22.04
 
 RUN apt update && \
     apt install -y curl zip unzip && \
-    curl -L -o /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.40.5/yq_linux_arm64 && \
+    curl -L -o /usr/local/bin/yq https://github.com/mikefarah/yq/releases/download/v4.40.5/yq_linux_amd64 && \
     chmod +x /usr/local/bin/yq && \
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-aarch64.zip" -o "awscliv2.zip" && \
+    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
     unzip awscliv2.zip && \
-    ./aws/install
+    ./aws/install && \
+    curl -L -o terraform.zip https://releases.hashicorp.com/terraform/1.7.2/terraform_1.7.2_linux_amd64.zip && \
+    unzip terraform.zip && \
+    mv terraform /usr/local/bin/terraform
 
 COPY run.sh /run.sh
 
